f784adfb6dcd518f840d35f5b3bf5dd7715b1c46
Yves-André Bergeron <devadar@gmail.com>
2020-04-19 16:02:44 -0400

    Améliorations du graph.

--- grapher.R
+++ grapher.R
@@ -3,17 +3,26 @@
 source("SetupData.R")
 
 
-locationInteressante <- c("Canada","Quebec","CanadaSansQuebec","Ontario", "Sweden", "Austria", "Norway", "Danemark", "Germany")
+locationInteressante <- c("Canada","Quebec","CanadaSansQuebec","United States",  "Sweden", "Germany", "Italy")
 
-dataOffSetDeathsPM <- 8
+dataOffSetDeathsPM <- 10
 
 dataGrapher <- dataGraph[total_deaths_per_million >= dataOffSetDeathsPM &
                            location %in% locationInteressante 
                            , joursEcoules := (1:.N), by = location]
 dataGrapher <- dataGrapher[joursEcoules > 0]
 
-graph <- ggplot(droplevels(dataGrapher),
+dernièreEntreeQueb <- max(dataGrapher$joursEcoules)
+
+graph <- ggplot(droplevels(dataGrapher[joursEcoules < dernièreEntreeQueb]),
                 aes(joursEcoules,
                     total_deaths_per_million,
                     colour = location))
-graph + geom_point() + geom_line()
+
+graph + geom_point() + geom_line()+
+  ggtitle("COVID19","Nb de morts par million selon le nb de jours") +
+  ylab("Nb de morts par million d'habitants") + 
+  xlab("Nb de jours depuis 10 morts/1M d'habitants") +
+  xlim(1,dernièreEntreeQueb)
+
+  
